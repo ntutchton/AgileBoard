abab4d75eff1ef0cc00889af6fd56d6597ab2468
Tutchton <la.James.Tutchton@sats.corp>
2017-10-24 10:24:02 -0600

    'sass_added'

--- agile-board-app/src/containers/App/App.js
+++ agile-board-app/src/containers/App/App.js
@@ -4,22 +4,23 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 //
 import RaisedButton from 'material-ui/RaisedButton';
 
-import logo from './logo.svg';
-import './App.css';
+import logo from '../../logo.svg';
+import styles from './App.scss';
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+        <div className={styles.app}>
+          <header className={styles.header}>
+            <img src={logo} className={styles.logo} alt="logo" />
+            <h1 className={styles.title}>Welcome to React</h1>
           </header>
-          <p className="App-intro">
+          <p className={styles.intro}>
             To get started, edit <code>src/App.js</code> and save to reload.
           </p>
           <RaisedButton label="Material UI" />
+
         </div>
       </MuiThemeProvider>
     );
--- agile-board-app/src/index.js
+++ agile-board-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import './index.css';
-import App from './App';
+import './index.scss';
+import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
 injectTapEventPlugin();
